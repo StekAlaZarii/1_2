793230863c8ba77f1270865340c2bcfe97f5db06
StekAlaZarii <101180506+StekAlaZarii@users.noreply.github.com>
2022-03-22 10:02:51 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println();
+
+    int l1 = 121;
+    double l2 = 20.5d;
+    char c = 'A';
+    boolean b = true;
+
+    System.out.println(l1);
+    System.out.println(l2);
+    System.out.println(c);
+    System.out.println(b);
+  }
+}
